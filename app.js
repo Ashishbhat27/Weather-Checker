00b9a447f08389327e182e1dc71bc31772383492
w3schools-test <ashishbhat27@gmail.com>
2023-08-01 16:09:18 +0530

    added bootstrap

--- app.js
+++ app.js
@@ -1,55 +1,92 @@
-document.getElementById('weatherForm').addEventListener('submit', async (event) => { // <-- make the callback async
+// Attach an event listener to the form submission event
+// Using the 'async' keyword allows us to use 'await' inside the function
+document.getElementById('weatherForm').addEventListener('submit', async (event) => {
+    // Prevent the form from submitting normally, which would cause the page to reload
     event.preventDefault();
 
+    // Get the city name from the input field
     const city = document.getElementById('city').value;
-    const apiKey = 'bdbea80df06a0a7b99ad3089bf2b10e2'; // replace with your OpenWeatherMap API key
+
+    // This is your OpenWeatherMap API key
+    const apiKey = 'bdbea80df06a0a7b99ad3089bf2b10e2';
 
     try {
-        // Fetch current weather
+        // Construct the URL for the weather API
         const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-        const weatherResponse = await fetch(weatherUrl); // <-- use await to wait for the promise to resolve
 
+        // Use the fetch API to get the weather data
+        // Using 'await' makes JavaScript wait until the Promise resolves
+        const weatherResponse = await fetch(weatherUrl);
+
+        // If the request was unsuccessful, throw an error
         if (!weatherResponse.ok) {
             throw new Error(`No weather found for ${city}`);
         }
 
-        const weatherData = await weatherResponse.json(); // <-- and again here
+        // Parse the JSON response
+        const weatherData = await weatherResponse.json();
 
+        // Destructure the response to get the needed data
         const { main, name, sys, weather } = weatherData;
+
+        // Get the element where the weather result will be displayed
         const weatherReport = document.getElementById('weatherResult');
+
+        // Display the weather data in a Bootstrap card
         weatherReport.innerHTML = `
-            <h2>${name}, ${sys.country}</h2>
-            <p>${weather[0].description}</p>
-            <p>Temp: ${main.temp} &deg;C</p>
-            <p>Humidity: ${main.humidity}%</p>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h2 class="card-title">${name}, ${sys.country}</h2>
+                    <p class="card-text">${weather[0].description}</p>
+                    <p class="card-text">Temp: ${main.temp} &deg;C</p>
+                    <p class="card-text">Humidity: ${main.humidity}%</p>
+                </div>
+            </div>
         `;
 
-        // Fetch 5 day forecast
+        // Construct the URL for the forecast API
         const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+
+        // Use the fetch API to get the forecast data
         const forecastResponse = await fetch(forecastUrl);
 
+        // If the request was unsuccessful, throw an error
         if (!forecastResponse.ok) {
             throw new Error(`No forecast found for ${city}`);
         }
 
+        // Parse the JSON response
         const forecastData = await forecastResponse.json();
 
+        // Filter the forecast data for readings at 18:00:00
         const dailyData = forecastData.list.filter(reading => reading.dt_txt.includes("18:00:00"));
 
+        // For each forecast reading...
         dailyData.forEach(item => {
+            // Convert the Unix timestamp to a date string
             const date = new Date(item.dt * 1000).toDateString();
+
+            // Destructure the reading to get the needed data
             const {main, weather} = item;
+
+            // Create a new Bootstrap Card for each forecast
             const forecastDiv = document.createElement('div');
+            forecastDiv.className = 'card mb-3';
             forecastDiv.innerHTML = `
-                <h3>${date}</h3>
-                <p>${weather[0].description}</p>
-                <p>Temp: ${main.temp} &deg;C</p>
-                <p>Humidity: ${main.humidity}%</p>
+                <div class="card-body">
+                    <h3 class="card-title">${date}</h3>
+                    <p class="card-text">${weather[0].description}</p>
+                    <p class="card-text">Temp: ${main.temp} &deg;C</p>
+                    <p class="card-text">Humidity: ${main.humidity}%</p>
+                </div>
             `;
+
+            // Append each forecast card to the weather report div
             weatherReport.appendChild(forecastDiv);
         });
 
     } catch (err) {
+        // If an error occurred, log it to the console and display it in the weather report div
         console.error(err);
         document.getElementById('weatherResult').textContent = err;
     }
